1916fe6261024fc7f4bd009f904f4f0b613d8306
unbeknownstx <110906750+unbeknownstx@users.noreply.github.com>
2026-09-11 15:28:27 -0700

    Export bounded MCP fabric primitives

--- mary/distributed/__init__.py
+++ mary/distributed/__init__.py
@@ -1,6 +1,17 @@
 """Distributed-runtime primitives for MaryV2."""
 from .capabilities import CapabilityDescriptor, capabilities_from_environment
 from .nodes import NodeDescriptor, NodeRegistry
+from .mcp_fabric import (
+    MCP_CAPABILITIES,
+    MCP_SERVER_CAPABILITIES,
+    MCPFabric,
+    MCPServerConfig,
+    mcp_dependency_available,
+    normalize_mcp_tool_name,
+    sanitize_mcp_result,
+    sanitize_mcp_task_args,
+    server_config_from_environment,
+)
 from .permissions import DeviceExecutionPermissions, default_permission_path
 from .tasks import (
     CapabilityTaskPlan,
@@ -17,6 +28,15 @@ __all__ = [
     "DeviceCapabilityTask",
     "DeviceTaskBroker",
     "DeviceExecutionPermissions",
+    "MCP_CAPABILITIES",
+    "MCP_SERVER_CAPABILITIES",
+    "MCPFabric",
+    "MCPServerConfig",
+    "mcp_dependency_available",
+    "normalize_mcp_tool_name",
+    "sanitize_mcp_result",
+    "sanitize_mcp_task_args",
+    "server_config_from_environment",
     "default_permission_path",
     "capabilities_from_environment",
     "preview_capability_task",
