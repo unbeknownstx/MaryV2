665af2fedf80aef7de7e87cf7790f72c9d77c315
unbeknownstx <110906750+unbeknownstx@users.noreply.github.com>
2026-09-11 21:01:25 -0700

    13.14 export MaryBench and optimizer interoperability

--- mary/learning/__init__.py
+++ mary/learning/__init__.py
@@ -1,5 +1,16 @@
 from .adapter_lab import AdapterConfiguration, AdapterEvaluation, AdapterLab, AdapterSpec
 from .model_candidates import ModelCandidate, ModelCandidateCatalog
+from .interop import (
+    MaryBenchRecord,
+    OptimizationProposal,
+    attach_results,
+    dspy_examples,
+    phoenix_rows,
+    promptfoo_tests,
+    proposal,
+    records_from_evaluation_set,
+    write_bundle,
+)
 
 __all__ = [
     "AdapterConfiguration",
@@ -8,6 +19,15 @@ __all__ = [
     "AdapterSpec",
     "ModelCandidate",
     "ModelCandidateCatalog",
+    "MaryBenchRecord",
+    "OptimizationProposal",
+    "attach_results",
+    "dspy_examples",
+    "phoenix_rows",
+    "promptfoo_tests",
+    "proposal",
+    "records_from_evaluation_set",
+    "write_bundle",
 ]
 
 from .adapter_runner import AdapterExperimentRunner, AdapterMix, EvalCase, ExperimentConfiguration, ExperimentResult, ExperimentSummary
