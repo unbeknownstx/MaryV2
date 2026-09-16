e29ed7fa914b0ab7d09f5257137b598a7322cc71
unbeknownstx <110906750+unbeknownstx@users.noreply.github.com>
2026-09-16 16:54:02 -0700

    Export Creator Lab planning primitives

--- mary/creative/__init__.py
+++ mary/creative/__init__.py
@@ -1,3 +1,4 @@
+from .creator_lab import build_creator_packet, effective_asset_description, normalize_asset
 from .production import (
     CharacterAnchor,
     CreativeReference,
@@ -12,6 +13,9 @@ from .services import CreativeServiceCapability, CreativeServiceRegistry
 from .studio import ProductionStudio
 
 __all__ = [
+    "build_creator_packet",
+    "effective_asset_description",
+    "normalize_asset",
     "CharacterAnchor",
     "CreativeReference",
     "ProductionFormat",
