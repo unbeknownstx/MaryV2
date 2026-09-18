cfb9bc1a4880c1ee2dfaf8aa6bec34adc1a154a8
unbeknownstx <110906750+unbeknownstx@users.noreply.github.com>
2026-09-17 21:03:27 -0700

    Export local knowledge fabric

--- mary/knowledge/__init__.py
+++ mary/knowledge/__init__.py
@@ -1,11 +1,15 @@
 from .world_context import WorldContextItem, WorldContextStore
 from .world_pulse import WorldPulseLane, WorldPulsePlanner
+from .fabric import KnowledgeFabric, KnowledgeHit, KnowledgePack
 
 __all__ = [
     "WorldContextItem",
     "WorldContextStore",
     "WorldPulseLane",
     "WorldPulsePlanner",
+    "KnowledgeFabric",
+    "KnowledgeHit",
+    "KnowledgePack",
 ]
 
 from .public_feeds import FeedHeadline, google_news_search_url, parse_rss_headlines
