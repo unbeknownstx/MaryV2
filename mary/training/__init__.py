69a564ec139cdcf6d4ca47d7eb8b866fd714d5ae
unbeknownstx <110906750+unbeknownstx@users.noreply.github.com>
2026-09-18 20:22:16 -0700

    Export Mary corpus mining inventory

--- mary/training/__init__.py
+++ mary/training/__init__.py
@@ -3,6 +3,7 @@ from .feedback import ResponseFeedback, ResponseFeedbackStore
 from .exporter import DatasetExportSummary, MaryTrainingDatasetExporter
 from .dataset_v1 import MaryDatasetV1Exporter, MaryDatasetV1Summary
 from .dataset_audit import MaryDatasetAuditReport, MaryDatasetV1Auditor
+from .corpus_mining import MaryCorpusMiner, MaryCorpusMiningReport
 from .novel_miner import NovelSceneCandidate, mine_mary_scenes, read_paragraphs, write_mining_bundle
 from .mlx_bundle import MlxTrainingProfile, PROFILES as MLX_TRAINING_PROFILES, prepare_mlx_bundle
 from .mlx_preflight import MlxBundlePreflight, inspect_mlx_bundle
@@ -13,6 +14,7 @@ __all__ = [
     "DatasetExportSummary", "MaryTrainingDatasetExporter",
     "MaryDatasetV1Exporter", "MaryDatasetV1Summary",
     "MaryDatasetAuditReport", "MaryDatasetV1Auditor",
+    "MaryCorpusMiner", "MaryCorpusMiningReport",
     "NovelSceneCandidate", "mine_mary_scenes", "read_paragraphs", "write_mining_bundle",
     "MlxTrainingProfile", "MLX_TRAINING_PROFILES", "prepare_mlx_bundle",
     "MlxBundlePreflight", "inspect_mlx_bundle",
