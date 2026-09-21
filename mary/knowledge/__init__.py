dfa6d09d6617203a05cbb9bcb5f6294d2fcec45a
unbeknownstx <110906750+unbeknownstx@users.noreply.github.com>
2026-09-20 22:10:22 -0700

    Export 13.77 knowledge evaluation evidence APIs

--- mary/knowledge/__init__.py
+++ mary/knowledge/__init__.py
@@ -1,7 +1,7 @@
 from .world_context import WorldContextItem, WorldContextStore
 from .world_pulse import WorldPulseLane, WorldPulsePlanner
 from .fabric import KnowledgeFabric, KnowledgeHit, KnowledgePack
-from .evaluation import KnowledgeEvaluationCase, KnowledgeEvaluationResult, KnowledgeFabricEvaluator, load_knowledge_evaluation_cases
+from .evaluation import (\n    KnowledgeEvaluationCase,\n    KnowledgeEvaluationResult,\n    KnowledgeFabricEvaluator,\n    KnowledgeEvaluationEvidenceStore,\n    knowledge_substrate_fingerprint,\n    knowledge_case_set_fingerprint,\n    load_knowledge_evaluation_cases,\n)
 
 __all__ = [
     "WorldContextItem",
